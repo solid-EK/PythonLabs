f37d75abe007fdd09bcdfa9d3ddba834ff1138f4
solid-EK <152191939+solid-EK@users.noreply.github.com>
2025-07-05 22:41:23 +0300

    Delete Практика_9 directory

--- Практика_9/zad1.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image, ImageFilter
-import os
-for i in os.listdir('input'):
-    img = Image.open(f'input/{i}')
-    img.load()
-    edge_img = img.filter(ImageFilter.FIND_EDGES)
-    filename = f"gray_{i}.png"
-    edge_img.save(f"output/{filename}")
