3df4b060cb7e4c1bae61dd901e520d7cea2f1604
solid-EK <152191939+solid-EK@users.noreply.github.com>
2025-07-05 22:46:22 +0300

    ver. 1

---
+++ Практика_11/zad1.py
@@ -0,0 +1,110 @@
+import requests
+from tkinter import *
+
+
+# API-ключ для OpenWeatherMap (замени на свой)
+APPID = '8292f3dddd09300935af82bdf6481fe6'
+city = "Санкт-Петербург"
+# URL для получения данных о погоде
+URL = "http://api.openweathermap.org/data/2.5/weather"
+
+# Параметры запроса (например, для города Москва)
+params = {
+    "q": city,  # Название города
+    "appid": APPID,  # Твой API-ключ
+    "units": "metric",  # Единицы измерения (метрическая система)
+    "lang": "ru"  # Язык для ответа
+}
+
+# Выполнение запроса
+response = requests.get(URL, params=params)
+
+# Проверка успешности запроса и вывод данных
+if response.status_code == 200:
+    data = response.json()
+    print(f"Погода в городе {data['name']} ({data['sys']['country']}):")
+    print(f"Температура: {data['main']['temp']}°C")
+    print(f"Ощущается как: {data['main']['feels_like']}°C")
+    print(f"Погодные условия: {data['weather'][0]['description']}")
+    print(f"Ветер: {data['wind']['speed']} м/с")
+    print(f"Видимость: {data['visibility'] / 100}%")
+else:
+    print(f"Ошибка {response.status_code}: {response.text}")
+
+
+def our_loc():
+    params = {
+        "q": "Санкт-Петербург",
+        "appid": APPID,
+        "units": "metric",
+        "lang": "ru"
+    }
+    response = requests.get(URL, params=params)
+    data = response.json()
+    output.config(text=f"Погода в городе {data['name']} ({data['sys']['country']}):\nТемпература: {data['main']['temp']}°C\n"
+                       f"Ощущается как: {data['main']['feels_like']}°C\nПогодные условия: {data['weather'][0]['description']}\n"
+                       f"Ветер: {data['wind']['speed']} м/с\nВидимость: {data['visibility'] / 100}%")
+
+
+def moscow():
+    params = {
+        "q": "Москва",
+        "appid": APPID,
+        "units": "metric",
+        "lang": "ru"
+    }
+    response = requests.get(URL, params=params)
+    data = response.json()
+    output.config(text=f"Погода в городе {data['name']} ({data['sys']['country']}):\nТемпература: {data['main']['temp']}°C\n"
+                       f"Ощущается как: {data['main']['feels_like']}°C\nПогодные условия: {data['weather'][0]['description']}\n"
+                       f"Ветер: {data['wind']['speed']} м/с\nВидимость: {data['visibility'] / 100}%")
+
+
+def tokyo():
+    params = {
+        "q": "Токио",
+        "appid": APPID,
+        "units": "metric",
+        "lang": "ru"
+    }
+    response = requests.get(URL, params=params)
+    data = response.json()
+    output.config(text=f"Погода в городе {data['name']} ({data['sys']['country']}):\nТемпература: {data['main']['temp']}°C\n"
+                       f"Ощущается как: {data['main']['feels_like']}°C\nПогодные условия: {data['weather'][0]['description']}\n"
+                       f"Ветер: {data['wind']['speed']} м/с\nВидимость: {data['visibility'] / 100}%")
+
+
+def by_coords(lat, lon):
+    params = {
+        "lat": lat,
+        "lon": lon,
+        "appid": APPID,
+        "units": "metric",
+        "lang": "ru"
+    }
+    response = requests.get(URL, params=params)
+    data = response.json()
+    output.config(
+        text=f"Погода по координатам {data['coord']['lat']}, {data['coord']['lon']} ({data['sys']['country']}):\nТемпература: {data['main']['temp']}°C\n"
+             f"Ощущается как: {data['main']['feels_like']}°C\nПогодные условия: {data['weather'][0]['description']}\n"
+             f"Ветер: {data['wind']['speed']} м/с\nВидимость: {data['visibility'] / 100}%")
+
+
+root = Tk()
+root.title('Погода')
+root.geometry('750x500')
+btn = Button(root, text='Санкт-Петербург', command=our_loc)
+btn.pack()
+btn2 = Button(root, text='Москва', command=moscow)
+btn2.pack()
+btn3 = Button(root, text='Токио', command=tokyo)
+btn3.pack()
+btn4 = Button(root, text='Узнать по координатам', command=lambda: by_coords(entry1.get(), entry2.get()))
+btn4.pack()
+entry1 = Entry()
+entry1.pack(anchor=N)
+entry2 = Entry()
+entry2.pack(anchor=S)
+output = Label(root)
+output.pack(side=TOP)
+root.mainloop()
---
+++ Практика_11/zad2.py
@@ -0,0 +1,21 @@
+import requests
+from tkinter import *
+response = requests.get('https://official-joke-api.appspot.com/random_joke')
+data = response.json()
+print(f"Случайная шутка:\n{data['setup']}\n{data['punchline']}")
+
+
+def joke():
+    response = requests.get('https://official-joke-api.appspot.com/random_joke')
+    data = response.json()
+    out.config(text=f"Случайная шутка:\n{data['setup']}\n{data['punchline']}")
+
+
+root = Tk()
+root.title('Случайная шутка')
+root.geometry('500x200')
+btn = Button(root, text='Посмеятся', command=joke)
+btn.pack()
+out = Label(root)
+out.pack()
+root.mainloop()
